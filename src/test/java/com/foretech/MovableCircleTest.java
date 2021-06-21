bb82ecd6015f5de37b40cf9e4f216711cd178a4b
AndreiHogea <andreihga@gmail.com>
2021-06-22 00:55:12 +0300

    - added MovableCircle Test

---
+++ src/test/java/com/foretech/MovableCircleTest.java
@@ -0,0 +1,24 @@
+package com.foretech;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovableCircleTest {
+
+    @Test
+    void moveUp() {
+    }
+
+    @Test
+    void moveDown() {
+    }
+
+    @Test
+    void moveLeft() {
+    }
+
+    @Test
+    void moveRight() {
+    }
+}
